24b572e936b950cb023612b4a391289e9c081b41
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2025-10-05 16:54:43 +0100

    sines trails

--- Assets/Scripts/Task/FollowTargetBhv.cs
+++ Assets/Scripts/Task/FollowTargetBhv.cs
@@ -10,10 +10,26 @@ public class FollowTargetBhv : TargetBhv
     [SerializeField, ReadOnly, Range(0, 1)]
     private float _normalizedDistance;
 
+    // Private fields
+    private Light _light;
+    private ParticleSystem _particles;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _light = this.GetComponent<Light>();
+        _particles = this.GetComponentInChildren<ParticleSystem>();
+    }
+
     private void Update()
     {
         float distance = Vector3.Distance(this.Position, TennisManager.Instance.Racket.Position);
         _normalizedDistance = Mathf.InverseLerp(0, maximumDistance, distance);
         base.ColorLerp(1.0f - _normalizedDistance);
+
+        _light.color = base.MeshRenderer.material.color;
+        var main = _particles.main;
+        main.startColor = base.MeshRenderer.material.color;
     }
 }
